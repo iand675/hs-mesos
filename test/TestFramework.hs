ed124bdb02288ad1c472a20bc282d267dc7e131c
Ian Duncan <ian.duncan@logos.com>
2014-03-28 23:32:45 -0700

    Attempt translation of 'no executor' framework example. Doesn't quite work.

--- test/TestFramework.hs
+++ test/TestFramework.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Char8 as C
+import Data.IORef
+import Data.Monoid ((<>))
+import System.Exit
 import System.Mesos.Scheduler
+import System.Mesos.Types
 
 cpusPerTask :: Int
 cpusPerTask = 1
@@ -7,21 +15,52 @@ cpusPerTask = 1
 memPerTask :: Int
 memPerTask = 32
 
-data TestScheduler = TestScheduler { role :: ByteString }
+data TestScheduler = TestScheduler
+  { role          :: C.ByteString
+  , totalTasks    :: Int
+  , tasksLaunched :: IORef Int
+  , tasksFinished :: IORef Int
+  }
 
-instance Scheduler TestScheduler where
-  registered _ _ _ = putStrLn "Registered!"
-  resourceOffers driver offers = do
-    putStr "."
+instance ToScheduler TestScheduler where
+  registered _ _ _ _ = putStrLn "Registered!"
+
+  resourceOffers s driver offers = do
     forM_ offers $ \offer -> do
-      let resources = parseResource ("cpus:" <> show cpusPerTask <> ";mem:" <> show memPerTask)
-  statusUpdate driver status = do
-    print $ "Task " <> taskID status <> " is in state " <> taskState status
-    if (taskState status == Finished)
-      then tasksFinished++
-      else return()
-    if (tasksFinished == totalTasks)
-      then return ()
-      else stop driver
-  errorMessage _ m = putStrLn m
+      putStrLn ("Starting task on " <> show (offerHostname offer))
+      status <- launchTasks
+        driver
+        [offerID offer]
+        [ TaskInfo "Task foo" (TaskID "foo") (offerSlaveID offer) (offerResources offer)
+            Nothing
+            (Just $ CommandInfo [] Nothing "echo hello")
+            Nothing
+        ]
+        (Filters Nothing)
+      print status
+      putStrLn "Launched tasks"
+    return ()
 
+  statusUpdate s driver status = do
+    putStrLn $ "Task " <> show (taskStatusTaskID status) <> " is in state " <> show (taskStatusState status)
+    when (taskStatusState status == Finished) $ do
+      count <- atomicModifyIORef' (tasksFinished s) (\x -> let x' = succ x in (x', x'))
+      when (count == totalTasks s) $ void $ stop driver False
+
+  errorMessage _ _ message = C.putStrLn message
+
+frameworkInfo :: C.ByteString -> C.ByteString -> FrameworkInfo
+frameworkInfo u n = FrameworkInfo u n Nothing Nothing Nothing Nothing Nothing
+
+main = do
+  role <- return "*" -- role to use when registering
+  master <- return "127.0.1.1:5050" -- ip:port of master to connect to
+  let info = (frameworkInfo "" "Test Framework (Haskell)") { frameworkRole = Just role }
+  scheduler <- TestScheduler "master" 5 <$> newIORef 0 <*> newIORef 0
+  status <- withSchedulerDriver scheduler info master Nothing $ \d -> do
+    status <- run d
+    stop d False
+    return status
+  if status /= Stopped
+    then exitFailure
+    else exitSuccess
