17c6c81bfe273a9def725c26ad42b49890cfa24f
Ian Duncan <ian.duncan@logos.com>
2014-03-31 11:50:43 -0700

    Add smart constructor for TaskStatus

---
+++ src/System/Mesos/TaskStatus.hs
@@ -0,0 +1,7 @@
+module System.Mesos.TaskStatus where
+import System.Mesos.Types
+
+taskStatus :: TaskID -> TaskState -> TaskStatus
+taskStatus tid ts = TaskStatus tid ts Nothing Nothing Nothing Nothing
+
+
